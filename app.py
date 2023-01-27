5d5d47377c3c731168f5d56fd51fec26ef7e10ec
Avaer Kazmer <a@modules.io>
2023-01-27 10:43:30 -0500

    Update prompting logic

--- app.py
+++ app.py
@@ -10,10 +10,16 @@ from torchvision import transforms
 from matplotlib import pyplot as plt
 
 from flask import Flask, request, Response
-
+import json
 
 app = Flask(__name__)
 
+# load model
+model = CLIPDensePredT(version='ViT-B/16', reduce_dim=64)
+model.eval()
+
+# non-strict, because we only stored decoder weights (not CLIP weights)
+model.load_state_dict(torch.load('weights/rd64-uni.pth', map_location=torch.device('cpu')), strict=False)
 
 @app.route('/predict', methods=['POST', 'OPTIONS'])
 def predict():
@@ -30,16 +36,13 @@ def predict():
         # print('got options 2')
         return response
 
-    # load model
-    model = CLIPDensePredT(version='ViT-B/16', reduce_dim=64)
-    model.eval()
-
-    # non-strict, because we only stored decoder weights (not CLIP weights)
-    model.load_state_dict(torch.load('weights/rd64-uni.pth', map_location=torch.device('cpu')), strict=False)
-
     body = request.get_data()
     # load and normalize image
     input_image = Image.open(io.BytesIO(body))
+
+    # parse prompts argument which should be encoded as json
+    prompts_json_string = request.args.get('prompts')
+    prompts = json.loads(prompts_json_string)
 
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -48,13 +51,16 @@ def predict():
     ])
     img = transform(input_image).unsqueeze(0)
 
-    prompts = ['a glass', 'something to fill', 'wood', 'a jar']
+    # prompts = ['a glass', 'something to fill', 'wood', 'a jar']
 
     # predict
+    preds = None
     with torch.no_grad():
         preds = model(img.repeat(4,1,1,1), prompts)[0]
 
     #img_from_tensor = Image.fromarray(input_image, 'RGB')
+
+    print(f"preds.shape: {preds.shape}")
 
     img_byte_arr = io.BytesIO()
     #img_from_tensor.save(img_byte_arr, format='PNG')
